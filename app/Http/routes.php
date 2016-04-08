7f86a168c9b812e57d0d9b7dc36a8ce8e2879e89
Michael M Kuhinica <taverneiro@gmail.com>
2016-04-08 15:24:48 +1000

    Initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// View with search box and button
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Stub api endpoints
+Route::group(['namespace' => 'Api\V1', 'prefix' => 'api/v1'], function()
+{
+  // JSON api endpoint to list the top artists in a specified country
+  Route::get('/artists/top/{country}', 'ArtistController@topByCountry');
+
+  // JSON api endpoint to list the top tracks of a specified artist
+  Route::get('/tracks/top/{artist}', 'TrackController@topByArtist');
+});
