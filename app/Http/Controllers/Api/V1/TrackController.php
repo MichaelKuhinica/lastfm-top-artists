fb9a7da8aab0f49283aa456d05a1eb6eae958060
Michael M Kuhinica <taverneiro@gmail.com>
2016-04-08 22:24:21 +1000

    Controller for tracks

---
+++ app/Http/Controllers/Api/V1/TrackController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Helpers\Contracts\LastFmClientContract;
+
+class TrackController extends Controller
+{
+    /**
+     * Returns a list of the top artists on the specified country
+     *
+     * @param  Request $request
+     * @param  string $country
+     * @return Response
+     */
+    public function topByArtist(Request $request, LastFmClientContract $lastFm, $artist) {
+      $page = $request->input('page') || 1;
+      $response = $lastFm->topTracksByArtist($artist, $page);
+      return response()->json($response);
+    }
+
+}
+
