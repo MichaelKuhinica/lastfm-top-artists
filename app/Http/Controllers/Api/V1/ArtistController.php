98cca5fa9a3815e96262dae237c536f233fd4714
Michael M Kuhinica <taverneiro@gmail.com>
2016-04-08 19:49:47 +1000

    Artist controller for top artists api endpoint

---
+++ app/Http/Controllers/Api/V1/ArtistController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Helpers\Contracts\LastFmClientContract;
+
+class ArtistController extends Controller
+{
+    /**
+     * Returns a list of the top artists on the specified country
+     *
+     * @param  Request $request
+     * @param  string $country
+     * @return Response
+     */
+    public function topByCountry(Request $request, LastFmClientContract $lastFm, $country) {
+      $page = $request->input('page') || 1;
+      $response = $lastFm->topArtistsByCountry($country, $page);
+      return response()->json($response);
+    }
+
+}
+
